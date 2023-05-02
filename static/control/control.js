f72eda8b34a1db9ae8873c5ba37d7b19432c2a83
afteralexxo <itsalextwod@gmail.com>
2023-05-02 00:34:59 -0700

    finally eyaln deresn

--- static/control/control.js
+++ static/control/control.js
@@ -16,42 +16,40 @@ const handleLoading = e => {
 
 window.addEventListener('load', handleLoading)
 
+  (function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+      button.addEventListener("click", function () {
+        try {
+          const activeBtn = document.querySelector(".active-btn");
+          if (activeBtn) {
+            activeBtn.classList.remove("active-btn");
+            console.log("Successfully removed the 'active-btn' class.");
+          } else {
+            throw new Error("Element with class 'active-btn' not found.");
+          }
+        } catch (error) {
+          console.error(error.message);
+          // Handle the error and provide feedback to the user if needed.
+        }
 
+        this.classList.add("active-btn");
+        try {
+          const active = document.querySelector(".active")
+          if (active) {
+            document.classList.remove("active");
+            console.log("Successfully removed the 'active' class.");
+          } else {
+            throw new Error("Element with class 'active' not found.");
+          }
+        } catch (error) {
+          console.error(error.message);
+          // Handle the error and provide feedback to the user if needed.
+        }
 
-//function that handle the navbar open and close in mobile devices
-const handleNavBarClik = (e) => {
-  isOpen = !isOpen
-  if (isOpen) {
-    nav.classList.add('fix')
-    logo.classList.add('hide')
-    navbar_list.classList.add('show')
-    navbar_list.classList.remove('hide')
-    icon.innerHTML = `<svg className="feather feather-x" fill="none" height="24" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-      <line x1="18" x2="6" y1="6" y2="18" />
-      <line x1="6" x2="18" y1="6" y2="18" />
-    </svg>`
-
-  }
-  else {
-    nav.classList.remove('fix')
-    logo.classList.remove('hide')
-    navbar_list.classList.remove('show')
-    navbar_list.classList.add('hide')
-    icon.innerHTML = swap
-    nav.removeEventListener('blur', handleNavBarClik, false)
-  }
-}
-
-(function () {
-  [...document.querySelectorAll(".control")].forEach(button => {
-    button.addEventListener("click", function () {
-      document.querySelector(".active-btn").classList.remove("active-btn");
-      this.classList.add("active-btn");
-      document.querySelector(".active").classList.remove("active");
-      document.getElementById(button.dataset.id).classList.add("active");
+        document.getElementById(button.dataset.id).classList.add("active");
+      })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+      document.body.classList.toggle("light-mode");
     })
-  });
-  document.querySelector(".theme-btn").addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
-  })
-})();
+  })();
