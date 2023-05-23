d1d4d6680ef49b5487634cc628373e8c70964a84
afteralexxo <itsalextwod@gmail.com>
2023-05-22 22:26:42 -0700

    adding scroll beahvior

--- static/control/control.js
+++ static/control/control.js
@@ -1,4 +1,5 @@
 (function () {
+  //scrolling beahvior when an element appear into the viewport add class and when an element leave the viewport remove class
   const cards = document.querySelectorAll('.card');
   const options = {
     rootMargin: '0px',
@@ -16,19 +17,22 @@
     observer.threshold = 0.2;
   };
 
-
+//when click the navbars go to that specific page
   [...document.querySelectorAll('.control')].forEach(el => {
     el.addEventListener('click', e => {
       e.preventDefault();
-      const url = 'https://afterog.netlify.app/index.html';
-      location.href = url + `#${e.target.getAttribute('data-id')}`;
+      let scrollTo = document.getElementById(e.target.getAttribute('data-id'));
+      scrollTo.scrollIntoView({ behavior: 'smooth' });
     });
   });
+
+//when cliked change the theme
   document.querySelector(".theme-btn").addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
   });
 })()
 
+//handle page loading
 const handleLoading = e => {
   document.querySelector('#loader').style.display = 'none';
   document.querySelector('.container').style.display = 'grid';
@@ -36,16 +40,62 @@ const handleLoading = e => {
 
 window.addEventListener('load', handleLoading);
 
+
+//handle form data
 document.querySelector('.submit-btn').addEventListener('click', event => {
   event.preventDefault();
-  const [fullName, email, subject, message] = [...document.querySelectorAll('.input-value')].map(element => element.value);
-  if (fullName && email && subject && message) {
-    const data = fetch('http://127.0.0.1:4000/getter')
-      .then(res => res.json())
-      .then(data => {
-        return data;
-      });
-
-    console.log(data);
+  const [fullName, email, subject, feedback] = [...document.querySelectorAll('.input-value')].map(element => element.value);
+  if (fullName && email && feedback) {
+    const data = {
+      fullName,
+      email,
+      feedback
+    };
+    fetchGetData(data);
+    return;
   }
 })
+
+const fetchGetData = (userData) => {
+
+  fetch('https://fair-gray-goat-gown.cyclic.app/getdata')
+    .then(response => response.json())
+    .then(data => {
+      console.log(userData);
+      console.log(data);
+      data.forEach(datum => {
+        if (datum.fullName === userData.fullName && datum.email === userData.email) {
+          console.log('name and email exist');
+          return;
+        }
+      })
+      fetchPostData(userData);
+      return;
+    }
+    )
+
+}
+
+const fetchPostData = (userData) => {
+
+  fetch('https://fair-gray-goat-gown.cyclic.app/setdata', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(responseData => {
+      console.log(responseData);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+}
